d34ba790fad21c580ae50d93473bf7330918371b
Simon Olofsson <s.olofsson@gmail.com>
2016-08-24 15:23:36 +0200

    Tabs can now be added. They are still empty though.

--- src/main/java/gUI/controllers/MainGuiController.java
+++ src/main/java/gUI/controllers/MainGuiController.java
@@ -1,7 +1,33 @@
 package gUI.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+
+import java.util.ArrayList;
+
 /**
  * Created by simpa2k on 2016-08-24.
  */
 public class MainGuiController {
+
+    @FXML
+    private Button tabButton;
+    @FXML
+    private TabPane tabPane;
+
+    private ArrayList<Tab> tabs = new ArrayList<Tab>();
+
+    @FXML
+    private void addTab(ActionEvent event) {
+       System.out.println("Clicked");
+
+       Tab tab = new Tab();
+       tab.setText("test");
+       tabPane.getTabs().add(tabPane.getTabs().size() - 1, tab);
+    }
+
+
 }
