c303d5c592d47eec9049a80d3019001e484a06f7
Simon Olofsson <s.olofsson@gmail.com>
2016-08-24 17:44:32 +0200

    ScraperTab now displays a dialog when watch() is called with an empty url input.

--- src/main/java/gUI/tab/ScraperTab.java
+++ src/main/java/gUI/tab/ScraperTab.java
@@ -1,12 +1,23 @@
 package gUI.tab;
 
+import Entites.SmallAd;
+import bevakare.BlocketScraper;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.VBoxBuilder;
 import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.util.Optional;
+import java.util.WeakHashMap;
 
 /**
  * Created by simpa2k on 2016-08-24.
@@ -30,20 +41,24 @@ public class ScraperTab extends Tab {
         urlAndSounds.getChildren().add(sounds);
 
         Button watchButton = new Button("Bevaka");
+
         watchButton.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
                 watch();
             }
         });
+
         gp.setConstraints(watchButton, 0, 2);
         gp.getChildren().add(watchButton);
 
         Button toSiteButton = new Button("Till sidan");
+
         toSiteButton.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
                 toSite();
             }
         });
+
         gp.setConstraints(toSiteButton, 1, 2);
         gp.getChildren().add(toSiteButton);
 
@@ -53,7 +68,18 @@ public class ScraperTab extends Tab {
 
     @FXML
     private void watch() {
-        //Scraper scraper = new Scraper();
+        String url = urlInput.getText();
+        if(url.trim().length() == 0) {
+            ButtonType buttonType = new ButtonType("Ok");
+            Dialog<String> dialog = new Dialog<>();
+            dialog.setContentText("Ingen url angiven, vänligen försök igen");
+            dialog.getDialogPane().getButtonTypes().add(buttonType);
+
+            dialog.show();
+        } else {
+            BlocketScraper scraper = new BlocketScraper(url);
+            /*SmallAd latestAd = scraper.scrapeNewest();*/
+        }
     }
 
     @FXML
